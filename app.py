b7122ce6ecfcc7f22ba20e2a8b258ac2e5476972
AndrejWp <m2403371@edu.misis.ru>
2025-11-22 19:14:51 +0300

    config: initial setup and constants

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template
+import requests
+import time
 
 app = Flask(__name__)
+
+
+API_KEY = 'ec589e598b051f59d5a0b8a098a07b61'
+BASE_URL = 'https://api.openweathermap.org/data/2.5/weather' #запрос на сервер
+CACHE_TIMEOUT = 300 #5 минут
 
 @app.route('/')
 def index():
